de20f9b312d2ab886af9a691386ac27432e43ae2
Brandon Fan <brandonfan1256@gmail.com>
2022-07-08 14:02:37 -0400

    update cloud build

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,8 +1,7 @@
 declare -a ROS_VERSIONS=( "foxy" "galactic" "humble" "rolling" )
 
 ORGANIZATION="microsoft"
-declare -a MODEL_VERSIONS=( "swin-tiny-patch4-window7-224", "swin-base-patch4-window7-224", 
-"swin-large-patch4-window7-224", "swin-small-patch4-window7-224", "swin-base-patch4-window12-384", "swin-large-patch4-window7-224")
+declare -a MODEL_VERSIONS=( "swin-tiny-patch4-window7-224" "swin-base-patch4-window7-224" "swin-large-patch4-window7-224" "swin-small-patch4-window7-224" "swin-base-patch4-window12-384" "swin-large-patch4-window7-224")
 
 for VERSION in "${ROS_VERSIONS[@]}"
 do
@@ -11,7 +10,7 @@ do
     ROS_VERSION="$VERSION"
     gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_MODEL_VERSION="$MODEL_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
     pids+=($!)
-    echo Dispached "$MODEL_VERSION" on ROS "$ROS_VERSION"
+    echo Dispatched "$MODEL_VERSION" on ROS "$ROS_VERSION"
   done
 done
 
